f373145aac8437b1d3fe05b69ec649a355e75717
Joana C. Costa <39338296+Joana-Cabral@users.noreply.github.com>
2025-03-11 17:13:54 +0000

    Update train_LISAD_TinyImageNet.sh

--- train_LISAD_TinyImageNet.sh
+++ train_LISAD_TinyImageNet.sh
@@ -1,4 +1,4 @@
 
 
-python3 main_TinyImageNet.py --network ResNet18 --batch-size 256 --projector 512-512-512 --gpu-device '0'
+python3 main_TinyImageNet.py --network ResNet18 --batch-size 256 --projector '512-512-512' --gpu-device '0'
 
